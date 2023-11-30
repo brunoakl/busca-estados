e0413e292fbbc7eb39540bf9e5ed372cae90ffbf
Bruno Machado Ferreira <bruno5289@hotmail.com>
2023-11-30 19:57:00 -0400

    Menus ajustados

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from eight_puzzle.busca_largura_eight_puzzle import BuscaLarguraEightPuzzle
 from eight_puzzle.busca_estrela_eight_puzzle import BuscaAEstrelaEightPuzzle
 from canibais_missionarios.busca_estrela_canibal_missionario import BuscaEstrelaCanibalMissionario
 from canibais_missionarios.busca_largura_canibal_missionario import BuscaLarguraCanibalMissionario
+from busca_minmax.busca_minmax_damas import *
+import sys
 
 class Menu:
     """
@@ -13,76 +15,87 @@ class Menu:
         """
         Menu inicial que irá começar
         """
-        try:
-            while True:
-                opcao = int(input("Escolha por favor qual problema deseja ver \n" +
-                        "1 - Missionários e Canibais\n" +
-                        "2 - 8 Puzzle\n" +
-                        "3 - Damas\n"))
-                match opcao:
-                    case 1:
-                        Menu.menu_canibal_missionario()
-                    case 2:
-                        Menu.menu_eight_puzzle()
-                    case 3:
-                        break
-                    case _:
-                        print("Opção Inválida")
-        except:
-            print("Valor inserido não é válido!")
-            Menu.menu()
+        while True:
+            opcao = int(input("Escolha um problema\n" +
+                    "1 - Missionários e Canibais\n" +
+                    "2 - 8 Puzzle\n" +
+                    "3 - Damas\n" +
+                    "9 - Sair\n"))
+            match opcao:
+                case 1: Menu.menu_canibal_missionario()
+                case 2: Menu.menu_eight_puzzle()
+                case 3: Menu.menu_damas()
+                case 9: sys.exit()
+                case _:
+                    print(str(opcao)+" é inválido. Tente novamente\n")
+                    Menu.menu()
+
 
     @classmethod
     def menu_canibal_missionario(self) -> None:
         """
         Menu para solucionar busca para solucionar problema do canibal e missionário
         """
-        try:
-            while True:
-                opcao = int(input("Escolha a técnica de busca que deseja usar \n" +
-                        "1 - Busca por Largura\n" +
-                        "2 - A Estrela\n" +
-                        "3 - Voltar\n"))
-                match opcao:
-                    case 1:
-                        BuscaLarguraCanibalMissionario.new().iniciar()
-                    case 2:
-                        BuscaEstrelaCanibalMissionario.new().iniciar()
-                    case 3:
-                        break
-                    case _:
-                        print("Opção Inválida")
-        except:
-            print("Valor inserido não é válido!")
-            Menu.menu_canibal_missionario()       
+        while True:
+            opcao = int(input("Escolha uma técnica de busca \n" +
+                    "1 - Busca por Largura\n" +
+                    "2 - A Estrela\n" +
+                    "8 - Voltar\n"+ 
+                    "9 - Sair\n"))
+            match opcao:
+                case 1: BuscaLarguraCanibalMissionario.new().iniciar()
+                case 2: BuscaEstrelaCanibalMissionario.new().iniciar()
+                case 8: break
+                case 9: sys.exit()
+                case _: print(str(opcao)+" é inválido. Tente novamente\n"); Menu.menu_canibal_missionario()
+
+                  
 
     @classmethod
     def menu_eight_puzzle(self) -> None:
         """
         Menu para solucionar busca para solucionar problema de 8 puzzle
         """
-        try:
-            while True:
-                opcao = int(input("Escolha a técnica de busca que deseja usar \n" +
-                        "1 - Busca por Largura\n" +
-                        "2 - A Estrela\n" +
-                        "3 - Voltar\n"))
-                lista_str = input("Informe como deseja que se inicie o 8 puzzle \n" +
-                              "exemplo: 3,4,1,2,5,6,9,0,7,8\n").split(',')
-                lista = (int(lista_str[0]), int(lista_str[1]), int(lista_str[2]), 
-                         int(lista_str[3]), int(lista_str[4]), int(lista_str[5]), 
-                         int(lista_str[6]), int(lista_str[7]), int(lista_str[8]))
-                match opcao:
-                    case 1:
-                        BuscaLarguraEightPuzzle.new(lista).iniciar()
-                    case 2:
-                        BuscaAEstrelaEightPuzzle.new(lista).iniciar()
-                    case 3:
-                        break
-                    case _:
-                        print("Opção Inválida")
-        except:
-            print("Valor inserido não é válido!")
-            Menu.menu_eight_puzzle() 
+        while True:
+            opcao = int(input("Escolha a técnica de busca que deseja usar \n" +
+                    "1 - Busca por Largura\n" +
+                    "2 - A Estrela\n" +
+                    "8 - Voltar\n" +
+                    "9 - Sair\n"))
+            
+            match opcao:
+                case 1 | 2:
+                    while True:
+                        lista_str = input("Informe como deseja que se inicie o 8 puzzle \n" +
+                                "exemplo: 3,4,1,2,5,6,9,0,7,8\n").split(',')
+                        lista = tuple(int(num) for num in lista_str)
+
+                        if opcao == 1:
+                            BuscaLarguraEightPuzzle.new(lista).iniciar()
+                        elif opcao == 2:
+                            BuscaAEstrelaEightPuzzle.new(lista).iniciar()
+                case 8: break
+                case 9: sys.exit()
+                case _:
+                    print(str(opcao) + " é inválido. Tente novamente\n")
+                    Menu.menu()
+                    
+    @classmethod
+    def menu_damas(self) -> None:
+        """
+        Menu para chamar o xadrez
+        """
+        while True:
+            opcao = int(input("Escolha uma técnica de busca \n" +
+                    "1 - Iniciar uma partida\n"+
+                    "8 - Voltar\n"+
+                    "9 - Sair\n"))
+            match opcao:
+                case 1: print("xadrez"); jogo_damas()
+                case 8: break
+                case 9: sys.exit()
+                case _: print(str(opcao)+" é inválido. Tente novamente\n"); Menu.menu_damas
+
+                    
 
 Menu.menu()
